5eee9c8c6ea45ae79da53713972ca763c2d7b929
Elvis Otieno <owuorelvis658@gmail.com>
2026-06-05 21:34:29 +0300

    reset project

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  'https://jddzmwukfibneqvgjwws.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpkZHptd3VrZmlibmVxdmdqd3dzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5MDg2MjksImV4cCI6MjA5NTQ4NDYyOX0.q9FvMXCphNqaV3LF__ynvsxgN98xujXAQ6oUZb7QSIY'
-)
